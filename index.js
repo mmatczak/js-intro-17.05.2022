aea7646ad6823b0da9159e76bc39f1b221c20168
Marek Matczak <marek.matczak79@gmail.com>
2022-05-17 14:03:36 +0200

    Ex. 1 added

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-console.log('Hello World!');
+// 1. Create class Order having id (Number), and businessKey (String) as properties
+// 2. Create class OrderService which stores a list of Orders; it can be initialized in the constructor
+// 3. Create CRUD (create read update delete) methods in the OrderService:
+// OrderService.add(order: Order) => void
+// OrderService.update(order: Order) => void
+// OrderService.delete(orderId: number) => void
+// OrderService.get() => Order[]
+// OrderService.findById(orderId: number) => Order | null
