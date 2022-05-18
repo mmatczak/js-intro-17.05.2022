b685a99241599edc472e27c7df8337f08e3577f7
Marek Matczak <marek.matczak79@gmail.com>
2022-05-18 09:29:59 +0200

    Solution

--- index.js
+++ index.js
@@ -1,3 +1,45 @@
-const firstNameElement =
-    document.querySelector('input#firstName');
-// https://modern-web-dev.github.io/js/index.html#/jsExercise
+class Employee {
+    constructor(firstName, lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+}
+
+class EmployeeFormComponent {
+    constructor(document, employee) {
+        this.document = document;
+        this.employee = employee || null;
+    }
+
+    onInit() {
+        this.saveOnFormSubmit();
+        this.initFormWithEmployeeIfAny();
+    }
+
+    saveOnFormSubmit() {
+        document.querySelector('form')
+            .addEventListener('submit',
+                // This binding is necessary to make `this` work in the callback
+                this.save.bind(this));
+    }
+
+    initFormWithEmployeeIfAny() {
+        if (this.employee) {
+            document.querySelector('input#firstName').value = this.employee.firstName;
+            document.querySelector('input#lastName').value = this.employee.lastName;
+        }
+    }
+
+    save(event) {
+        event.preventDefault();
+        const formElement = event.target;
+        const firstNameElement = formElement['firstName'];
+        const lastNameElement = formElement['lastName'];
+        this.employee = new Employee(firstNameElement.value, lastNameElement.value);
+    }
+}
+
+const component = new EmployeeFormComponent(document, new Employee('John', 'Smith'));
+component.onInit();
+
+setTimeout(() => console.log(component.employee), 5000);
